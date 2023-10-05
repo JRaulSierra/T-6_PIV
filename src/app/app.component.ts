5496ffa5d9faef1834206ecc9858ffb722e8a477
Raul Sierra <jsierra@labymed.com.gt>
2023-10-05 09:12:34 -0600

    Creating the app container and the components structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'T-6_P-IV';
+  showForm: boolean = false;
+  showTable: boolean = false;
 }
